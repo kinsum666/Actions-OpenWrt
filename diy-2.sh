f8fa85b838b6094c56c2ce51d256ebd116fa359a
kinsum <79512621+kinsum666@users.noreply.github.com>
2026-06-27 10:38:13 +0800

    Update diy-2.sh

--- diy-2.sh
+++ diy-2.sh
@@ -13,3 +13,7 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.50.1/g' package/base-files/files/bin/config_generate
 
+sed -i 's/jdcloud_re-ss-01/jdcloud-re-ss-01/g' target/linux/qualcommax/image/ipq60xx.mk
+# 同时修改 .config 中的设备符号
+sed -i 's/CONFIG_TARGET_DEVICE_qualcommax_ipq60xx_DEVICE_jdcloud_re-ss-01=y/CONFIG_TARGET_DEVICE_qualcommax_ipq60xx_DEVICE_jdcloud-re-ss-01=y/g' .config
+
